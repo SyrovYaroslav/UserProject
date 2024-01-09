daefddc7e44477b727a20a0833823d669f51db64
Yaroslav <syrovatkaaroslav@gmail.com>
2024-01-09 13:54:13 +0200

    init commit

---
+++ src/main/java/org/example/userproject1/UserProject1Application.java
@@ -0,0 +1,13 @@
+package org.example.userproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProject1Application.class, args);
+	}
+
+}
